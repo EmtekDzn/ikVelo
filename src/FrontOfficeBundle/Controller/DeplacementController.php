eb83086815f6b0713ceb3f80ad5ac635aa02884a
Gableb <gableb@hotmail.fr>
2018-10-24 10:27:07 +0200

    Front : deplacements list done, Started create deplacement

--- src/FrontOfficeBundle/Controller/DeplacementController.php
+++ src/FrontOfficeBundle/Controller/DeplacementController.php
@@ -4,29 +4,41 @@ namespace FrontOfficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DeplacementController extends Controller
 {
     /**
-     * @Route("/list/{userId}")
+     * @Route("/list/{userId}", name="deplacements_list")
      */
     public function listAction($userId = 2)
     {
-        // $user = $this->getDoctrine()->getRepository('FrontOfficeBundle:User')->find($userId);
-        $deplacements = $this->getDoctrine()->getRepository('FrontOfficeBundle:User')->findBy(array('user_id_DANSLABDD' => $userId));
+        $user = $this->getDoctrine()->getRepository('FrontOfficeBundle:User')->find($userId);
+        $deplacements = $this->getDoctrine()->getRepository('FrontOfficeBundle:Deplacement')->findBy(array('user' => $user));
         return $this->render('FrontOfficeBundle:DeplacementController:list.html.twig', array(
             'deplacements' => $deplacements
         ));
     }
 
     /**
-     * @Route("/create")
+     * @Route("/create/{userId}")
      */
-    public function createAction()
+    public function createAction(Request $request, $userId = 2)
     {
-        return $this->render('FrontOfficeBundle:DeplacementController:create.html.twig', array(
-            // ...
-        ));
+        $createForm = $this->createForm('BackOfficeBundle\Form\DeplacementType');
+        $createForm->handleRequest($request);
+
+
+        if ($createForm->isSubmitted() && $createForm->isValid()) {
+            $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
+            $deplacement->setUser1($user);
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('deplacements_list');
+        }
+
+        return $this->render('FrontOfficeBundle:DeplacementController:create.html.twig', array('form' => $createForm->createView()));
     }
 
     /**
