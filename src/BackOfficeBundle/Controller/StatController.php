d31c103feda479aa4a0da26b681dc42325cf95e2
Emtek <lagadecpa@gmail.com>
2018-12-18 13:23:08 +0100

    stats

--- src/BackOfficeBundle/Controller/StatController.php
+++ src/BackOfficeBundle/Controller/StatController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
 
 /**
  * Stats controller.
@@ -22,8 +23,12 @@ class StatController extends Controller
      */
     public function indexAction()
     {
-
         $em = $this->getDoctrine()->getManager();
+        
+        $months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+        $societes = $em->getRepository('BackOfficeBundle:Societe')->findAll();
+        $societesArray = array();
+        $tabfinal = array();
 
         $usersPerCity = $em->createQuery(
             "SELECT t2.ville ,COUNT(t1.ville)
@@ -43,10 +48,35 @@ class StatController extends Controller
             GROUP BY s.societe"
         )->getResult();
 
-    
+        $kmPerMonthPerUserPerSociety = $em->createQuery(
+            "SELECT s.societe, d.mois, u.nom, SUM(dj.nbKm) AS Total_km
+            FROM BackOfficeBundle:Societe s, BackOfficeBundle:User u, BackOfficeBundle:Deplacement d, BackOfficeBundle:DeplacementJour dj
+            WHERE s.id = u.societe
+            AND u.id = d.user
+            AND d.id = dj.deplacement
+            AND d.annee = 2018
+            GROUP BY s.societe, d.mois, u.id"
+        )->getResult();
+
+        foreach ($kmPerMonthPerUserPerSociety as $key => $value) {  
+            for ($i=1 ; $i < sizeof($months)+1 ; $i++) {
+                $tabfinal[$value["societe"]][$i] = array();
+            }
+        }
+        
+        foreach ($kmPerMonthPerUserPerSociety as $key => $value) {  
+            $tabfinal[$value["societe"]][$value["mois"]][$value["nom"]] = $value["Total_km"];
+        }
+
+        foreach ($societes as $key => $value) {
+            array_push($societesArray, $value->getSociete());
+        }
+
         return $this->render('BackOfficeBundle:Stat:index.html.twig', array(
             'usersPerCity' => $usersPerCity,
             'kmAndUsersPerSociety' => $kmAndUsersPerSociety,
+            'kmPerMonthPerUserPerSociety' => $tabfinal,
+            'months' => $months
         ));
     }
 }
