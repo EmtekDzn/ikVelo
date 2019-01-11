d49783892951c3ad71d6c5c3ec57232b69f4a5b3
Emtek <lagadecpa@gmail.com>
2019-01-11 17:51:56 +0100

    c bon

--- src/BackOfficeBundle/Controller/StatController.php
+++ src/BackOfficeBundle/Controller/StatController.php
@@ -29,6 +29,7 @@ class StatController extends Controller
         $societes = $em->getRepository('BackOfficeBundle:Societe')->findAll();
         $societesArray = array();
         $tabfinal = array();
+        $currentYear = date("Y");
 
         $usersPerCity = $em->createQuery(
             "SELECT t2.ville ,COUNT(t1.ville)
@@ -39,31 +40,55 @@ class StatController extends Controller
             GROUP BY t2.ville"
         )->getResult();
 
-        $kmAndUsersPerSociety = $em->createQuery(
-            "SELECT s.societe, COUNT(u.societe) AS Nombre_d_utilisateurs, SUM(dj.nbKm) AS Total_km
+        $kmPerSociety = $em->createQuery(
+            "SELECT s.societe, SUM(dj.nbKm)
+            AS Total_km
             FROM BackOfficeBundle:Societe s, BackOfficeBundle:User u, BackOfficeBundle:Deplacement d, BackOfficeBundle:DeplacementJour dj
-            WHERE s.id = u.societe 
+            WHERE s.id = u.societe
             AND u.id = d.user
             AND d.id = dj.deplacement
             GROUP BY s.societe"
         )->getResult();
 
+        $usersPerSociety = $em->createQuery(
+            "SELECT t2.societe ,COUNT(t1.societe)
+            AS Nombre_d_utilisateurs
+            FROM BackOfficeBundle:User t1
+            INNER JOIN BackOfficeBundle:Societe t2
+            WHERE t1.societe = t2.id
+            GROUP BY t2.societe"
+        )->getResult();
+
+        
         $kmPerMonthPerUserPerSociety = $em->createQuery(
             "SELECT s.societe, d.mois, u.nom, SUM(dj.nbKm) AS Total_km
             FROM BackOfficeBundle:Societe s, BackOfficeBundle:User u, BackOfficeBundle:Deplacement d, BackOfficeBundle:DeplacementJour dj
             WHERE s.id = u.societe
             AND u.id = d.user
             AND d.id = dj.deplacement
-            AND d.annee = 2018
+            AND d.annee = ".$currentYear."
             GROUP BY s.societe, d.mois, u.id"
-        )->getResult();
+            )->getResult();
+
+        
+        $kmAndUsersPerSociety = $usersPerSociety;
+            
+        foreach ($usersPerSociety as $keyups => $ups) {
+            $kmAndUsersPerSociety[$keyups]["Total_km"] = 0;
+            
+            foreach ($kmPerSociety as $key => $value) {
+                if(isset($kmPerSociety[$key]["Total_km"]) && $kmAndUsersPerSociety[$keyups]["societe"] == $kmPerSociety[$key]["societe"]) {
+                    $kmAndUsersPerSociety[$keyups]["Total_km"] = $kmPerSociety[$key]["Total_km"];
+                }
+            }
+        }
 
         foreach ($kmPerMonthPerUserPerSociety as $key => $value) {  
             for ($i=1 ; $i < sizeof($months)+1 ; $i++) {
                 $tabfinal[$value["societe"]][$i] = array();
             }
         }
-        
+    
         foreach ($kmPerMonthPerUserPerSociety as $key => $value) {  
             $tabfinal[$value["societe"]][$value["mois"]][$value["nom"]] = $value["Total_km"];
         }
